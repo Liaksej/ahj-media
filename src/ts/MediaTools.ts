76cccdbc33ba1374f5b3ae4624b3b94349bb6b09
Liaksej <tarusov.liaksej@gmail.com>
2023-08-25 12:25:30 +0200

    Add timer and cancel recording functionality
    Added a timer to the MediaRecorder for user feedback and the ability to cancel recording. A timer now runs every 1000ms, indicating the recording's duration. Added a handler for cancel recording; when cancel is clicked, the recording stops, mediaElement is removed, and timer is cleared. This improves the UX by providing more control and information to the user.

--- src/ts/MediaTools.ts
+++ src/ts/MediaTools.ts
@@ -84,13 +84,37 @@ export class MediaTools {
     const recorder = new MediaRecorder(stream);
     const chunks: Blob[] = [];
 
+    let timer: number;
+    let witchButton: string;
+
     const mediaElement: Element | undefined =
       typeOfStream === "video"
         ? Array.from(document.querySelectorAll(".video_container")).at(-1)
         : Array.from(document.querySelectorAll(".audio_container")).at(-1);
 
+    const videoButton = document.querySelector(".video");
+    const audioButton = document.querySelector(".audio");
+
     recorder.addEventListener("start", () => {
       console.log("start");
+      let counter: number = 0;
+      timer = setInterval(() => {
+        counter++;
+        if (recorder.state === "recording") {
+          const lastTimer = document.querySelector(".timer");
+          if (lastTimer) {
+            lastTimer.remove();
+          }
+          const minutes = Math.floor(counter / 60);
+          const seconds = counter % 60;
+          const element = document.createElement("div");
+          element.classList.add("timer");
+          element.textContent = `${minutes
+            .toString()
+            .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+          videoButton?.insertAdjacentElement("afterend", element);
+        }
+      }, 1000);
     });
 
     recorder.addEventListener("dataavailable", (event) => {
@@ -101,44 +125,75 @@ export class MediaTools {
       const blob = new Blob(chunks);
       console.log("stop");
       if (mediaElement instanceof HTMLMediaElement) {
+        if (witchButton === "stop") {
+          mediaElement.src = URL.createObjectURL(blob);
+          mediaElement.setAttribute("width", "300");
+          mediaElement.setAttribute("controls", "");
+          if (typeOfStream === "video") {
+            vault.at(-1)!.video = blob;
+            console.log(vault.at(-1)!.video);
+          } else {
+            vault.at(-1)!.audio = blob;
+            console.log(vault.at(-1)!.audio);
+          }
+        }
+        if (witchButton === "cancel") {
+          mediaElement.remove();
+        }
+
         mediaElement.srcObject = null;
-        mediaElement.src = URL.createObjectURL(blob);
-        mediaElement.setAttribute("width", "300");
 
-        mediaElement.setAttribute("controls", "");
-
-        if (typeOfStream === "video") {
-          vault.at(-1)!.video = blob;
-          console.log(vault.at(-1)!.video);
-        } else {
-          vault.at(-1)!.audio = blob;
-          console.log(vault.at(-1)!.audio);
+        clearInterval(timer);
+        if (videoButton?.nextElementSibling?.classList.contains("timer")) {
+          videoButton?.nextElementSibling?.remove();
         }
       }
     });
 
-    document
-      .querySelector(".video")
-      ?.addEventListener("click", async (event) => {
-        if (
-          event.target instanceof HTMLElement &&
-          event.target.classList.contains("fa-stop")
-        ) {
-          recorder.stop();
-          activeStream.forEach((stream) => {
-            stream.getTracks().forEach((track) => {
-              track.stop();
-            });
+    videoButton?.addEventListener("click", async (event) => {
+      if (
+        event.target instanceof HTMLElement &&
+        event.target.classList.contains("fa-stop")
+      ) {
+        witchButton = "stop";
+        recorder.stop();
+        activeStream.forEach((stream) => {
+          stream.getTracks().forEach((track) => {
+            track.stop();
           });
+        });
 
-          if (event.target.parentElement) {
-            event.target.parentElement.innerHTML = `<i class="fa-solid fa-video"></i>`;
-            document.querySelector(
-              ".audio",
-            )!.innerHTML = `<i class="fa-solid fa-microphone"></i>`;
-          }
+        if (event.target.parentElement) {
+          event.target.parentElement.innerHTML = `<i class="fa-solid fa-video"></i>`;
+          document.querySelector(
+            ".audio",
+          )!.innerHTML = `<i class="fa-solid fa-microphone"></i>`;
         }
-      });
+      }
+    });
+
+    audioButton?.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (
+        event.target instanceof HTMLElement &&
+        event.target.classList.contains("fa-xmark")
+      ) {
+        witchButton = "cancel";
+        recorder.stop();
+        activeStream.forEach((stream) => {
+          stream.getTracks().forEach((track) => {
+            track.stop();
+          });
+        });
+
+        if (event.target.parentElement) {
+          event.target.parentElement.innerHTML = `<i class="fa-solid fa-microphone"></i>`;
+          document.querySelector(
+            ".video",
+          )!.innerHTML = `<i class="fa-solid fa-video"></i>`;
+        }
+      }
+    });
 
     recorder.start();
   }
